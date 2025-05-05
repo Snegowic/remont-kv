26bccf423a596c2e1a6d4fe0fdbb14dff9da1410
Snegowic <nk205936@gmail.com>
2025-05-05 17:00:26 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,33 @@
-const slider = document.querySelectorAll('.slide'); 
+/* вкладки */
+let btns = document.querySelectorAll('.btn');
+let texts = document.querySelectorAll('.text');
+btns.forEach((btn, index) => {
+  btn.onclick = () => {
+    document.querySelector('.btn.active').classList.remove('active');
+    btn.classList.add('active');
+    document.querySelector('.text.active').classList.remove('active');
+    texts[index].classList.add('active');
+  };
+});
+
+
+/* слайдер */
+const slides = document.querySelectorAll('.slide');
 let currentIndex = 0;
 
-function switchSlides(){
-  slider[currentIndex].classList.remove('active');
-  currentIndex ++;
-  if(currentIndex >= slider.length){
+function switchSlides() {
+  slides[currentIndex].classList.remove('active');
+  currentIndex++;
+  if (currentIndex >= slides.length) {
     currentIndex = 0;
   }
-  slider[currentIndex].classList.add('active');
+  slides[currentIndex].classList.add('active');
 }
 setInterval(switchSlides, 5000);
 
 
-
-// Функция для переключения аккордеона
-function Accordion(button) {
-  const item = button.parentElement;
-  const content = button.nextElementSibling;
-
-  button.classList.toggle('active');
-  content.classList.toggle('active');
-
-  // Закрываем другие открытые элементы аккордеона
-  const allItems = document.querySelectorAll('.portfolio__block');
-  allItems.forEach(accordionItem => {
-      if (accordionItem !== item) {
-          accordionItem.querySelector('.portfolio__item').classList.remove('active');
-          accordionItem.querySelector('.accordion-content').classList.remove('active');
-      }
-  });
+/* Аккордион */
+function toggleAccordion(header){
+  let content = header.nextElementSibling;
+  content.classList.toggle("show2");
 }
